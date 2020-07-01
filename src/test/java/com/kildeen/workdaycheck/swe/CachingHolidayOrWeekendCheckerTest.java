971fd789e2ed9992d0b6f81b2c9ec0e8124731be
karl.kilden <karl.kilden@gmail.com>
2020-07-01 09:19:13 +0200

    Docs bug

--- src/test/java/com/kildeen/workdaycheck/swe/CachingHolidayOrWeekendCheckerTest.java
+++ src/test/java/com/kildeen/workdaycheck/swe/CachingHolidayOrWeekendCheckerTest.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * HolidayOrWeekendChecker is not currently not in use pending cache eviction strategy during production.
+ * {@link CachingHolidayOrWeekendChecker} is not currently not in use pending cache eviction strategy during production.
  * But using {@link HolidayOrWeekendCheckerTest#perf()} A  performance enhancement could be noted (60% on my machine).
  * Makes sense since the same date will occur many many times as many daily passages is the root cause for having a toll in the first place
  * The cache should probably be warmed-up as we know all keys will be used eventually. E.g populate all possible dayOfYear eagerly.
